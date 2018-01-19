8fcb77e548ec0edfa059b25c2e2f02be5e171157
Collin Edwards <collinedwards1579@gmail.com>
2018-01-19 01:12:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a, b){
+  return a + b
+}
+
+function subtract(a, b){
+  return a - b
+}
+
+function multiply(a, b){
+  return a * b
+}
+
+function divide (a, b){
+  return a / b
+}
+
+function inc(n){
+  return ++inc
+}
+
+function dec(n){
+  return --inc
+}
+
+function makeInt(n){
+  return parseInt(n)
+}
+
+add (1, 2)
+subtract (10, 5)
+multiply (2, 2)
+divide(500, 100)
+inc(10)
+dec(10)
+makeInt("10")
